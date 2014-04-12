a9aef1269fd4a842a56f33fa2673426959bf574c
Thameera Senanayaka <thameera123@gmail.com>
2014-04-12 18:44:30 +0530

    Setup snippet calling structure
    Signed-off-by: Thameera Senanayaka <thameera123@gmail.com>

--- public/js/app.js
+++ public/js/app.js
@@ -20,7 +20,7 @@ drillApp.config(['$locationProvider', '$routeProvider', function($locationProvid
       templateUrl: 'scala.html',
       controller: 'ScalaHomeCtrl'
     })
-    .when('/:lang/play', {
+    .when('/:lang/p/:cat', {
       templateUrl: 'play.html',
       controller: 'PlayCtrl'
     })
@@ -29,12 +29,35 @@ drillApp.config(['$locationProvider', '$routeProvider', function($locationProvid
     });
 }]);
 
+drillApp.service('CommService', ['$http', function($http) {
+  this.fetchSnippets = function(lang, cat, cb) {
+    $http.get('snippets/' + lang + '/' + cat).success(cb);
+  };
+}]);
+
+drillApp.service('UtilService', [function() {
+  this.getRandomSnippet = function(snippets) {
+    var r = Math.floor(Math.random() * snippets.length);
+    return snippets[r];
+  };
+}]);
+
 drillApp.controller('HomeCtrl', ['$scope', function($scope) {
   //
 }]);
 
 drillApp.controller('JSHomeCtrl', ['$scope', function($scope) {
-  //
+  $scope.categories = [
+    {cat: 'basics', title: 'Basics'},
+    {cat: 'controlstructures', title: 'Control Structures'},
+    {cat: 'strings', title: 'Strings'},
+    {cat: 'arrays', title: 'Arrays'},
+    {cat: 'functions', title: 'Functions'},
+    {cat: 'json', title: 'JSON'},
+    {cat: 'es6', title: 'ES6'},
+    {cat: 'jquery', title: 'jQuery'},
+    {cat: 'lodash', title: 'Lo-dash'},
+  ];
 }]);
 
 drillApp.controller('PyHomeCtrl', ['$scope', function($scope) {
@@ -45,7 +68,15 @@ drillApp.controller('ScalaHomeCtrl', ['$scope', function($scope) {
   //
 }]);
 
-drillApp.controller('PlayCtrl', ['$scope', function($scope) {
-  //
+drillApp.controller('PlayCtrl', ['$scope', '$routeParams', 'CommService', 'UtilService', function($scope, $routeParams, Comm, Util) {
+  $scope.snippets = [];
+  $scope.curSnippet = '';
+  $scope.lang = $routeParams.lang;
+  $scope.cat = $routeParams.cat;
+
+  Comm.fetchSnippets($scope.lang, $scope.cat, function(data) {
+    $scope.snippets = data;
+    $scope.curSnippet = Util.getRandomSnippet($scope.snippets);
+  });
 }]);
 
